41f60a79ac9c652b43b27d31cd87a6fe5bbbaa80
Toan Slan <batoan1221@gmail.com>
2013-11-27 10:07:37 +0700

    Initial Commit

---
+++ AFNetworkingJSON/AFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AFAppDelegate.h
+//  AFNetworkingJSON
+//
+//  Created by Toan Slan on 11/27/13.
+//  Copyright (c) 2013 Toan Slan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
